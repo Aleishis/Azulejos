ef14c2e23ce971a1f85b31013e93defede4039f1
aleishis <mecedesval@gmail.com>
2025-10-01 20:21:04 -0700

    Se modifican botones existentes y se modifica archivo JS

--- resources/azulejos.js
+++ resources/azulejos.js
@@ -8,58 +8,21 @@ document.addEventListener("DOMContentLoaded", function(){
     // Declaramos un evento "click" a ese boton
     btnOk.addEventListener("click", function(){
         // Declaramos una variable que representa el input del HTML
-        var value1Number = parseFloat(document.getElementById("input-value1").value);
+        var largo = parseFloat(document.getElementById("largo").value);
         
-        var value2Number = parseFloat(document.getElementById("input-value2").value)
+        var ancho = parseFloat(document.getElementById("ancho").value)
 
 
         // isNaN = is not a number
-        if (isNaN(value1Number) || isNaN(value2Number)){
-            alert("Estas estupido")
+        if (isNaN(largo) || isNaN(ancho)){
+            alert("Solo puedes ingresar numeros")
         }
+
+        
 
         var resultado = value1Number + value2Number
         // Mostamos alerta del navegador con el contenido del input
         alert("El resultado es: " + resultado)
 
     })
-
-    var btnRestar = document.getElementById("btn-restar");
-
-    btnRestar.addEventListener("click", function(){
-        // Declaramos una variable que representa el input del HTML
-        var value1Number = parseFloat(document.getElementById("input-value1").value);
-        
-        var value2Number = parseFloat(document.getElementById("input-value2").value)
-
-
-        // isNaN = is not a number
-        if (isNaN(value1Number) || isNaN(value2Number)){
-            alert("Estas estupido")
-        }
-
-        var resultado = value1Number - value2Number
-        // Mostamos alerta del navegador con el contenido del input
-        alert("El resultado es: " + resultado)
-    })
-
-    var btnMultiplicar = document.getElementById("btn-multiplicar");
-
-    btnMultiplicar.addEventListener("click", function(){
-        // Declaramos una variable que representa el input del HTML
-        var value1Number = parseFloat(document.getElementById("input-value1").value);
-        
-        var value2Number = parseFloat(document.getElementById("input-value2").value)
-
-
-        // isNaN = is not a number
-        if (isNaN(value1Number) || isNaN(value2Number)){
-            alert("Estas estupido")
-        }
-
-        var resultado = value1Number * value2Number
-        // Mostamos alerta del navegador con el contenido del input
-        alert("El resultado es: " + resultado)
-    })
-    
 })
